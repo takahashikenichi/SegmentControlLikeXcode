e808ecc8f65e3844efa060c600263797d51d6d46
Kenichi Takahashi <takahashi.kenichi@gmail.com>
2022-06-27 00:32:29 +0900

    Initial Commit

--- SegmentedControlLikeXcode/ContentView.swift
+++ SegmentedControlLikeXcode/ContentView.swift
@@ -1,19 +1,45 @@
-//
-//  ContentView.swift
-//  SegmentedControlLikeXcode
-//
-//  Created by Kenichi Takahashi on 2022/06/27.
-//
-
 import SwiftUI
 
+enum SideMenuSelection: String, CaseIterable  {
+    case folders = "folder"
+    case bubble = "bubble.left"
+    case tag = "tag"
+}
+
 struct ContentView: View {
+    
+    @State var sideMenuStatus : SideMenuSelection = .folders
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Spacer()
+                Picker(selection: $sideMenuStatus, label: EmptyView(), content: {
+                    ForEach(SideMenuSelection.allCases, id: \.self) { sideMenuSelection in
+                        Image(systemName: sideMenuSelection.rawValue).font(.title).tag(sideMenuSelection)
+                    }
+                })
+                .pickerStyle(SegmentedPickerStyle())
+                .frame(width: 180, height:26, alignment: .center)
+                
+                Spacer()
+            }
+                        
+            HStack {
+                Spacer()
+                ForEach(SideMenuSelection.allCases, id: \.self) { sideMenuSelection in
+                    Button(action: {
+                        sideMenuStatus = sideMenuSelection
+                    }, label: {
+                        Image(systemName: sideMenuSelection.rawValue)
+                            .symbolVariant(sideMenuStatus == sideMenuSelection ? .fill : .none)
+                            .foregroundColor(sideMenuStatus == sideMenuSelection ? .accentColor: .none)
+                    })
+                    .buttonStyle(.borderless)
+                    .frame(width: 24)
+                }
+                Spacer()
+            }
         }
     }
 }
