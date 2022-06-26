1708cb640d690411a3699b79fa40ad1c044f5bcb
Kenichi Takahashi <takahashi.kenichi@gmail.com>
2022-06-27 00:30:33 +0900

    Initial Commit

---
+++ SegmentedControlLikeXcode/SegmentedControlLikeXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedControlLikeXcodeApp.swift
+//  SegmentedControlLikeXcode
+//
+//  Created by Kenichi Takahashi on 2022/06/27.
+//
+
+import SwiftUI
+
+@main
+struct SegmentedControlLikeXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
